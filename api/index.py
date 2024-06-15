a153e4e139e6dc5d3f042e5e4d2c76b13b1925f9
G2 <166596134+Gtwo2@users.noreply.github.com>
2024-06-15 19:42:37 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def intro():
+    return '/scrape?url=<target_url>'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/scrape')
+def scrape():
+    target_url = request.args.get('url')
+    if not target_url:
+        return "ERROR : URL not specified", 400
+
+    try:
+        response = requests.get(target_url)
+        response.raise_for_status()
+        html = response.text
+        return html
+    except requests.exceptions.RequestException as e:
+        return f"ERROR : {str(e)}", 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
