fab372d929cc28ff0eb40aebbb33e3e6a5e28955
G2 <166596134+Gtwo2@users.noreply.github.com>
2024-06-16 11:36:24 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,30 +1,25 @@
 from flask import Flask, request
 import requests
-from bs4 import BeautifulSoup
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def intro():
-    return '/scrape?url=target_url'
+    return '/scrape?url=target url'
+    return 'https://github.com/Gtwo2/html-scraper/blob/main/README.md'
 
 @app.route('/scrape')
 def scrape():
     target_url = request.args.get('url')
     if not target_url:
         return "ERROR : URL not specified", 400
-
     try:
         response = requests.get(target_url)
         response.raise_for_status()
         html = response.text
-
-        soup = BeautifulSoup(html, 'html5lib')
-        standardized_html = soup.prettify()
-
-        return standardized_html
+        return html
     except requests.exceptions.RequestException as e:
         return f"ERROR : {str(e)}", 500
-
 if __name__ == '__main__':
     app.run()
